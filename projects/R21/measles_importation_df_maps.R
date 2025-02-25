5f374a5377e56b31d063201bbe7c21d9dc9b1ba8
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2025-02-25 13:35:42 -0500

    create and write dfs, import and begin mapping

---
+++ projects/R21/measles_importation_df_maps.R
@@ -0,0 +1,49 @@
+#dependencies
+library(tidyverse)
+library(tidyr)
+library(readxl)
+library(sirfunctions)
+library(ggplot2)
+
+global.ctry <- sirfunctions::load_clean_ctry_sp()
+long.global.ctry <- sirfunctions::load_clean_ctry_sp(type = "long")
+
+#read in and process data
+#region.imports.count <- readxl::read_excel("C:/Users/uic3/Desktop/DATT/measles_importation.xlsx", 
+ #                                          sheet = "region_imports")
+#region.import.per <- readxl::read_excel("C:/Users/uic3/Desktop/DATT/measles_importation.xlsx", 
+ #                                       sheet = "region_imports_per")
+#import.cases.count <- readxl::read_excel("C:/Users/uic3/Desktop/DATT/measles_importation.xlsx", 
+ #                                        sheet = "import_cases")
+#import.cases.per <- readxl::read_excel("C:/Users/uic3/Desktop/DATT/measles_importation.xlsx", 
+ #                                      sheet = "import_cases_per")
+
+#measles.import.data.list <- list()
+#measles.import.data.list$region.imports.count <- region.imports.count
+#measles.import.data.list$region.import.percentage <- region.import.per
+#measles.import.data.list$import.cases.count <- import.cases.count
+#measles.import.data.list$import.cases.percentage <- import.cases.per
+
+#sirfunctions::edav_io(io = "write", obj = measles.import.data.list, default_dir = "GID/GIDMEA/giddatt", 
+ #                     file_loc = "data_clean/measles.import.data.list.rds")
+
+#import data list
+measles.import.data.list <- sirfunctions::edav_io(io = "read", default_dir = "GID/GIDMEA/giddatt", 
+                                                  file_loc = "data_clean/measles.import.data.list.rds")
+
+#make importation case counts long and attach to 2024 country shapes by region
+#using 2024 country shapes because maps are at region level so year specific countries aren't that important 
+#compared to keeping shapes constant for year to year maps
+region.imports.count <- measles.import.data.list$region.imports.count |>
+  tidyr::pivot_longer("Total":"2024") |>
+  dplyr::rename("year" = "name") |>
+  dplyr::arrange(year)
+
+ctry.24.shapes <- long.global.ctry |>
+  dplyr::filter(active.year.01 == 2024)
+
+region.counts.shape <- dplyr::left_join(ctry.24.shapes, region.imports.count, by = c("WHO_REGION" = "Source_WHO_region"))
+  
+#total importations
+ggplot(region.counts.shape |> dplyr::filter(year == "Total")) +
+  
