0ca9e1203b197afc4248dc9a555f52ea248b10b3
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2025-03-13 11:56:22 -0400

    fixing country names

--- projects/R20/mr_incidence_imports_maps.R
+++ projects/R20/mr_incidence_imports_maps.R
@@ -8,19 +8,28 @@ library(ggarrow)
 #all vpd
 vpd.case.data <- sirfunctions::edav_io(io = "read", default_dir = NULL, file_loc = "GID/GIDMEA/giddatt/data_clean/country_vpd_year_casedata.rds")
 
-#subset all vpd to measles cases
-measles.cases <- vpd.case.data |>
-  filter(vpd == "Measles" & variable == "cases")
+#county shapes, filter to 2025 to keep maps consistent
+ctry.shapes <- load_clean_ctry_sp(type = "long") |>
+  select(WHO_REGION, ADM0_NAME, STARTDATE, ENDDATE, active.year.01) |>
+  filter(active.year.01 == 2025)
 
+#subset all vpd to measles cases, fixing names to conform with shapes
+measles.cases <- vpd.case.data |>
+  filter(vpd == "Measles" & variable == "cases") |>
+  mutate(country = toupper(country_name), 
+         country = case_when(country == "CÔTE D'IVOIRE" ~ "COTE D IVOIRE",
+                             country == ""
+         ) |>
+  left_join(ctry.shapes, by = c("country" = "ADM0_NAME"))
+  
+missed.ctry <- measles.cases |>
+  filter(is.na(active.year.01))
 #mr import data
 mr_data <- read_excel("reference/mr_import_01_24.xlsx")
-
-#county shapes
-ctry.shapes <- load_clean_ctry_sp(type = "long") |>
-  select(WHO_REGION, ADM0_NAME, STARTDATE, ENDDATE, active.year.01)
+  
 
 #prepping data for mapping
 mr_data <- left_join(mr_data, ctry.shapes, by = c("country" = "ADM0_NAME"))
 
 measles.18.19 <- measles.cases |>
-  filter(year %in% c("2018", "2019"))
+  filter(year %in% c("2018", "2019")) 
