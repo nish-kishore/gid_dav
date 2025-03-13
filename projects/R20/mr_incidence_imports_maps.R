ac06e452e13c472b265c92b2f82eeb74cf486f6e
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2025-03-13 13:58:38 -0400

    fix country names, create 18/19 df and sum cases by country

--- projects/R20/mr_incidence_imports_maps.R
+++ projects/R20/mr_incidence_imports_maps.R
@@ -16,20 +16,27 @@ ctry.shapes <- load_clean_ctry_sp(type = "long") |>
 #subset all vpd to measles cases, fixing names to conform with shapes
 measles.cases <- vpd.case.data |>
   filter(vpd == "Measles" & variable == "cases") |>
-  mutate(country = toupper(country_name), 
+  mutate(country = toupper(country_name),
          country = case_when(country == "CÔTE D'IVOIRE" ~ "COTE D IVOIRE",
-                             country == ""
-         ) |>
-  left_join(ctry.shapes, by = c("country" = "ADM0_NAME"))
-  
-missed.ctry <- measles.cases |>
-  filter(is.na(active.year.01))
+                             country == "KOSOVO (IN ACCORDANCE WITH UN SECURITY COUNCIL RESOLUTION 1244 (1999))" ~ "KOSOVO", 
+                             country == "NETHERLANDS (KINGDOM OF THE)" ~ "NETHERLANDS", 
+                             country == "NORTHERN MARIANA ISLANDS" ~ "NORTHERN MARIANA ISLANDS (COMMONWEALTH OF THE)", 
+                             country == "SINT MAARTEN (DUTCH PART)" ~ "SINT MAARTEN", 
+                             country == "UNITED KINGDOM OF GREAT BRITAIN AND NORTHERN IRELAND" ~ "THE UNITED KINGDOM", 
+                             .default = country)) 
+
 #mr import data
 mr_data <- read_excel("reference/mr_import_01_24.xlsx")
   
-
 #prepping data for mapping
 mr_data <- left_join(mr_data, ctry.shapes, by = c("country" = "ADM0_NAME"))
 
+#calculate sum for years specified by country and join to shapes
 measles.18.19 <- measles.cases |>
-  filter(year %in% c("2018", "2019")) 
+  filter(year %in% c("2018", "2019")) |>
+  select(-year) |>
+  group_by(country) |>
+  mutate(value = sum(value)) |>
+  ungroup() |>
+  unique() |>
+  
